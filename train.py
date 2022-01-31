547b9a7f04afafdfd83b54379ffbe33575e12388
outinim <mehdi.outini@gmail.com>
2022-01-31 23:00:50 +0100

    mlflow run experiment

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from importlib.resources import path
 import os
 import warnings
 import sys
+import datetime
 
 import pandas as pd
 import numpy as np
@@ -23,16 +24,13 @@ import dvc.api
 
 path = "data/wine-quality.csv"
 repo = "/home/outini/mlops/dvc-mlflow-test"
-version = "v1"
+version = "v2"
 
 data_url = dvc.api.get_url(
     path=path,
     repo=repo,
     rev=version,
 )
-
-
-mlflow.set_experiment("demo")
 
 
 def eval_metrics(actual, pred):
@@ -55,50 +53,53 @@ if __name__ == "__main__":
     # Read the wine-quality csv file from the remote repository
     data = pd.read_csv(data_url, sep=",")
 
-    # Log data params
-    mlflow.log_param("data_url", data_url)
-    mlflow.log_param("data_version", version)
-    mlflow.log_param("input_rows", data.shape[0])
-    mlflow.log_param("input_cols", data.shape[1])
+    mlflow.set_experiment("demo")
+    with mlflow.start_run():
 
-    # Split the data into training and test sets. (0.75, 0.25) split.
-    train, test = train_test_split(data)
+        # Log data params
+        mlflow.log_param("data_url", data_url)
+        mlflow.log_param("data_version", version)
+        mlflow.log_param("input_rows", data.shape[0])
+        mlflow.log_param("input_cols", data.shape[1])
 
-    # The predicted column is "quality" which is a scalar from [3, 9]
-    train_x = train.drop(["quality"], axis=1)
-    test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
+        # Split the data into training and test sets. (0.75, 0.25) split.
+        train, test = train_test_split(data)
 
-    # Log artifacts: columns used for modeling
-    cols_x = pd.DataFrame(list(train_x.columns))
-    cols_x.to_csv("features.csv", header=False, index=False)
-    mlflow.log_artifact("features.csv")
+        # The predicted column is "quality" which is a scalar from [3, 9]
+        train_x = train.drop(["quality"], axis=1)
+        test_x = test.drop(["quality"], axis=1)
+        train_y = train[["quality"]]
+        test_y = test[["quality"]]
 
-    cols_y = pd.DataFrame(list(train_y.columns))
-    cols_y.to_csv("targets.csv", header=False, index=False)
-    mlflow.log_artifact("targets.csv")
+        # Log artifacts: columns used for modeling
+        cols_x = pd.DataFrame(list(train_x.columns))
+        cols_x.to_csv("features.csv", header=False, index=False)
+        mlflow.log_artifact("features.csv")
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+        cols_y = pd.DataFrame(list(train_y.columns))
+        cols_y.to_csv("targets.csv", header=False, index=False)
+        mlflow.log_artifact("targets.csv")
 
-    # with mlflow.start_run():
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-    lr.fit(train_x, train_y)
+        alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+        l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    # predicted_qualities = lr.predict(test_x)
+        # Training
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
 
-    # (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        # Evaluation
+        predicted_qualities = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    # print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-    # print("  RMSE: %s" % rmse)
-    # print("  MAE: %s" % mae)
-    # print("  R2: %s" % r2)
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
 
-    # mlflow.log_param("alpha", alpha)
-    # mlflow.log_param("l1_ratio", l1_ratio)
-    # mlflow.log_metric("rmse", rmse)
-    # mlflow.log_metric("r2", r2)
-    # mlflow.log_metric("mae", mae)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
 
-    # mlflow.sklearn.log_model(lr, "model")
+        mlflow.sklearn.log_model(lr, "model")
